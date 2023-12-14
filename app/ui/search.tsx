a08f3bebf8078330aaf140f243c63fac9b198e73
abel <vnponce8@gmail.com>
2023-12-14 11:04:11 -0600

    feat(invoices): replace search with query

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,9 +11,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
     const params = new URLSearchParams(searchParams);
 
     if (term) {
-      params.set('search', term);
+      params.set('query', term);
     } else {
-      params.delete('search');
+      params.delete('query');
     }
 
     replace(`${pathname}?${params.toString()}`);
@@ -30,7 +30,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('search')?.toString()}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
