3be73070ef71f82d3916176aba077e94edbff88f
abel <vnponce8@gmail.com>
2023-12-14 08:17:36 -0600

    fix(font): missing lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,7 @@
 import { Montserrat, Lusitana } from 'next/font/google';
 
 export const montserrat = Montserrat({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight: '400' });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
