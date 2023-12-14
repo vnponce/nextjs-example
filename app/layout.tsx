7b53e08a062e6f244ff4c0ab1e650f87a12848c8
abel <vnponce8@gmail.com>
2023-12-14 07:10:29 -0600

    feat(component): simple footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className="py-10 flex justify-center items-center">Made with Love</footer>
+      </body>
     </html>
   );
 }
