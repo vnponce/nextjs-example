8ec433ce56129a38e7990944f62a0ede343bb0b7
abel <vnponce8@gmail.com>
2023-12-14 08:02:53 -0600

    fix(dashboard): layout typo

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-export default function Layout({ children }: { childre: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
