4e9a9ab90e1240b250d8c26012b1bbd17a1530df
abel <vnponce8@gmail.com>
2023-12-14 08:19:44 -0600

    fix(font): missing inter

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,9 @@
-import { Montserrat, Lusitana } from 'next/font/google';
+import { Montserrat, Lusitana, Inter } from 'next/font/google';
 
 export const montserrat = Montserrat({ subsets: ['latin'] });
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
+
+export const inter = Inter({ subsets: ['latin'] });
